8efd960778e1a606e6131cae8241641c0a195853
JP Lataquin <jp.lataquin@gmail.com>
2026-05-02 22:29:20 +0800

    fix

--- resources/views/livewire/battle-room.blade.php
+++ resources/views/livewire/battle-room.blade.php
@@ -60,23 +60,23 @@
         <!-- Team A Column -->
         <div class="col-6" style="min-width: 0;">
             <div class="text-center mb-4 team-name-container" 
-                 wire:key="team-name-b-{{ md5($battle->team_name_b) }}"
+                 wire:key="team-name-a-{{ md5($battle->team_name_a) }}"
                  x-data="{ 
                     overflowing: false,
                     checkOverflow() {
-                        if(this.$refs.nakedB) {
-                            this.overflowing = this.$refs.nakedB.scrollWidth > this.$el.clientWidth;
+                        if(this.$refs.nakedA) {
+                            this.overflowing = this.$refs.nakedA.scrollWidth > this.$el.clientWidth;
                         }
                     }
                  }" 
                  x-init="setTimeout(() => checkOverflow(), 200)"
                  @resize.window="checkOverflow()">
                 <div :class="{ 'team-name-scroll': overflowing }">
-                    <h4 class="orbitron text-magenta mb-0 d-inline-block" :class="{ 'pe-5': overflowing }" title="{{ $battle->team_name_b }}">
-                        <span x-ref="nakedB">{{ $battle->team_name_b }}</span>
+                    <h4 class="orbitron text-cyan mb-0 d-inline-block" :class="{ 'pe-5': overflowing }" title="{{ $battle->team_name_a }}">
+                        <span x-ref="nakedA">{{ $battle->team_name_a }}</span>
                     </h4>
-                    <h4 x-show="overflowing" class="orbitron text-magenta mb-0 d-inline-block pe-5" title="{{ $battle->team_name_b }}">
-                        {{ $battle->team_name_b }}
+                    <h4 x-show="overflowing" class="orbitron text-cyan mb-0 d-inline-block pe-5" title="{{ $battle->team_name_a }}">
+                        {{ $battle->team_name_a }}
                     </h4>
                 </div>
             </div>
