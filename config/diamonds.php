4cd47f03799c058ea13bb6e6e3e88293ab5bbea6
JP Lataquin <jp.lataquin@gmail.com>
2026-05-04 12:06:41 +0800

    fix

--- config/diamonds.php
+++ config/diamonds.php
@@ -5,35 +5,4 @@ return [
         'template_creation' => 3,
         'forging' => 2,
     ],
-    'packages' => [
-        'package_10' => [
-            'id' => 'package_10',
-            'diamonds' => 10,
-            'price' => 50,
-            'currency' => 'PHP',
-            'name' => 'Starter Pack',
-        ],
-        'package_20' => [
-            'id' => 'package_20',
-            'diamonds' => 20,
-            'price' => 100,
-            'currency' => 'PHP',
-            'name' => 'Fighter Pack',
-        ],
-        'package_50' => [
-            'id' => 'package_50',
-            'diamonds' => 50,
-            'price' => 120,
-            'currency' => 'PHP',
-            'name' => 'Pro Pack',
-        ],
-        'package_100' => [
-            'id' => 'package_100',
-            'diamonds' => 100,
-            'price' => 130,
-            'promo_price' => 110,
-            'currency' => 'PHP',
-            'name' => 'Rich Kid Pack',
-        ],
-    ],
 ];
